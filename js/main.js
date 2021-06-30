28639b575a02159867ca633435d02ec0141f72ba
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-30 19:01:26 +0300

    window event listener and scrollY

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('arfdhg');
+const backToTopDOM = document.querySelector('.back-to-top');
+const headerDOM = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 150) {
+        headerDOM.classList.add('sticky')
+    }
+    else {
+        headerDOM.classList.remove('sticky')
+    }
+    if (window.scrollY > 500) {
+        backToTopDOM.classList.remove('hide');
+
+    } else {
+        backToTopDOM.classList.add('hide');
+
+    }
+})
